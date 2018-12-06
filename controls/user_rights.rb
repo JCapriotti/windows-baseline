f6573452c0fcb5a3b919cd2be6bf009648421f29
Jason Capriotti <jason.capriotti@gmail.com>
2018-12-06 15:18:21 -0600

    Add some user_rights

--- controls/user_rights.rb
+++ controls/user_rights.rb
@@ -2,6 +2,14 @@
 
 title 'User Rights Assignment'
 
+#
+# SID Reference:
+#
+# S-1-5-32-544 = Adminsitrators
+# S-1-5-32-545 = Users
+# S-1-5-32-546 = Guests
+#
+
 control 'cis-access-cred-manager-2.2.1' do
   impact 0.7
   title '2.2.1 Set Access Credential Manager as a trusted caller to No One'
@@ -46,5 +54,69 @@ control 'cis-adjust-memory-quotas-2.2.5' do
     its('SeIncreaseQuotaPrivilege') { should include 'S-1-5-19' }
     its('SeIncreaseQuotaPrivilege') { should include 'S-1-5-20' }
     its('SeIncreaseQuotaPrivilege') { should include 'S-1-5-32-544' }
+  end
+end
+
+control 'cis-allow-log-on-locally' do
+  impact 0.7
+  title '(L1) Ensure "Allow log on locally" is set to "Administrators"'
+  tag cis: ['windows_2016_1607:2.2.7']
+  describe security_policy do
+    its('SeInteractiveLogonRight') { should eq ['S-1-5-32-544'] }
+  end
+end
+
+control 'cis-back-up-files-and-directories' do
+  impact 0.7
+  title '(L1) Ensure "Back up files and directories" is set to "Administrators"'
+  tag cis: ['windows_2016_1607:2.2.10']
+  describe security_policy do
+    its('SeBackupPrivilege') { should eq ['S-1-5-32-544'] }
+  end
+end
+
+control 'deny-log-on-as-a-batch-job' do
+  impact 0.7
+  title '(L1) Ensure "Deny log on as a batch job" to include "Guests"'
+  tag cis: ['windows_2016_1607:2.2.22']
+  describe security_policy do
+    its('SeDenyBatchLogonRight') { should include 'S-1-5-32-546' }
+  end
+end
+
+control 'deny-log-on-as-a-service' do
+  impact 0.7
+  title '(L1) Ensure "Deny log on as a service" to include "Guests"'
+  tag cis: ['windows_2016_1607:2.2.23']
+  describe security_policy do
+    its('SeDenyServiceLogonRight') { should include 'S-1-5-32-546' }
+  end
+end
+
+control 'deny-log-on-locally' do
+  impact 0.7
+  title '(L1) Ensure "Deny log on locally" to include "Guests"'
+  tag cis: ['windows_2016_1607:2.2.24']
+  describe security_policy do
+    its('SeDenyInteractiveLogonRight') { should include 'S-1-5-32-546' }
+  end
+end
+
+control 'restore-files-and-directories' do
+  impact 0.7
+  title '(L1) Ensure "Restore files and directories" is set to "Administrators"'
+  tag cis: ['windows_2016_1607:2.2.45']
+  describe security_policy do
+    its('SeRestorePrivilege') { should eq ['S-1-5-32-544'] }
+  end
+end
+
+control 'shut-down-the-system' do
+  impact 0.7
+  title '(L1) Ensure "Shut down the system" is set to "Administrators, Users"'
+  tag cis: ['windows_2016_1607:2.2.46']
+  describe security_policy do
+    its('SeShutdownPrivilege') { should include 'S-1-5-32-544' }
+    its('SeShutdownPrivilege') { should include 'S-1-5-32-545' }
   end
 end
